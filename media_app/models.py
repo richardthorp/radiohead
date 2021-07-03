63f3693f54070f1e458640fc7dc1952f77d73303
richardthorp <richardthorp@hotmail.co.uk>
2021-07-03 13:00:03 +0000

    Move images to seperate folders, add back link to media

--- media_app/models.py
+++ media_app/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class Single(models.Model):
     title = models.TextField(blank=False)
-    image = models.ImageField(blank=False, null=False)
+    image = models.ImageField(blank=False, null=False,
+                              upload_to="single_covers")
     album = models.ForeignKey('shop.Album', null=True,
                               on_delete=models.SET_NULL)
     video_url = models.URLField(blank=False, null=False)
--- shop/models.py
+++ shop/models.py
@@ -9,12 +9,11 @@ class Album(models.Model):
                                    decimal_places=2)
     vinyl_price = models.DecimalField(blank=False, null=False, max_digits=5,
                                       decimal_places=2)
-    image = models.ImageField(blank=False,
-                              null=False)
+    image = models.ImageField(blank=False, null=False, upload_to="album_covers")
     spotify_url = models.URLField(blank=False, null=False)
 
     def __str__(self):
-        return self.name
+        return self.title
 
 
 class Product(models.Model):
