811d99af84051142ffdd61a3bf9c966ad22ba94f
richardthorp <richardthorp@hotmail.co.uk>
2021-07-05 11:41:44 +0000

    Add profile update logic and edit template

--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from .models import Profile
+# from .models import Profile
 from .forms import ProfileForm
 
 
 @login_required
 def profile(request):
+    profile = request.user.profile
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+        else:
+            print('Form not valid')
+    else:
+        form = ProfileForm(instance=profile)
     context = {
-        'profile': Profile.objects.get(user=request.user),
-        'form': ProfileForm(instance=request.user.profile),
+        'profile': profile,
+        'form': form,
     }
+
     return render(request, 'profiles/profile.html', context)
