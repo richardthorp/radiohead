8ced4e4a4e9214c83414f33567a6727fda33d29d
richardthorp <richardthorp@hotmail.co.uk>
2021-07-28 16:46:16 +0000

    Add validation to quantity on update or add_to_bag

--- bag/views.py
+++ bag/views.py
@@ -9,75 +9,95 @@ def view_bag(request):
 
 
 def add_to_bag(request, product_id):
-    if request.POST.get('format'):
-        album = get_object_or_404(Album, pk=product_id)
-        album_name = album.title
-        format = request.POST.get('format')
-        size = False
-    else:
-        product = get_object_or_404(Product, pk=product_id)
-        product_name = product.name
-        size = request.POST.get('size')
-        album = False
-
-    quantity = int(request.POST.get('quantity'))
-
     bag = request.session.get('bag', {})
+    if request.method == 'POST':
+        if request.POST.get('format'):
+            album = get_object_or_404(Album, pk=product_id)
+            album_name = album.title
+            format = request.POST.get('format')
+            size = False
+            type = 'album'
+        else:
+            product = get_object_or_404(Product, pk=product_id)
+            product_name = product.name
+            size = request.POST.get('size')
+            album = False
+            type = 'product'
 
-    if album:
-        if album_name in bag.keys():
-            if format in bag[album_name].keys():
-                bag[album_name][format] += quantity
-            else:
-                bag[album_name][format] = quantity
-        else:
-            bag[album_name] = {
-                'type': 'album',
-                format: quantity,
-            }
-    elif size:
-        if product_name in bag.keys():
-            if size in bag[product_name].keys():
-                bag[product_name][size] += quantity
-            else:
-                bag[product_name][size] = quantity
-        else:
-            bag[product_name] = {
-                'type': 'sized',
-                size: quantity
-            }
-    else:
-        if product_name in bag.keys():
-            bag[product_name] += quantity
-        else:
-            bag[product_name] = quantity
+        quantity = int(request.POST.get('quantity'))
+        if quantity < 1:
+            messages.error(request,
+                           'You can not add less than one item to your bag!')
+            if type == 'album':
+                return redirect(reverse('shop_detail',
+                                        args=[type, album.id]))
+            if type == 'product':
+                return redirect(reverse('shop_detail',
+                                        args=[type, product.id]))
 
-    request.session['bag'] = bag
-    messages.success(request, 'Item successfully added to your bag.')
-    # return render(request, 'bag/view_bag.html')
-    return redirect(reverse('view_bag'))
+        if album:
+            if album_name in bag.keys():
+                if format in bag[album_name].keys():
+                    bag[album_name][format] += quantity
+                else:
+                    bag[album_name][format] = quantity
+            else:
+                bag[album_name] = {
+                    'type': 'album',
+                    format: quantity,
+                }
+        elif size:
+            if product_name in bag.keys():
+                if size in bag[product_name].keys():
+                    bag[product_name][size] += quantity
+                else:
+                    bag[product_name][size] = quantity
+            else:
+                bag[product_name] = {
+                    'type': 'sized',
+                    size: quantity
+                }
+        else:
+            if product_name in bag.keys():
+                bag[product_name] += quantity
+            else:
+                bag[product_name] = quantity
+
+        request.session['bag'] = bag
+        messages.success(request, 'Item successfully added to your bag.')
+        return redirect(reverse('view_bag'))
+    else:  # Request method is not post, redirect request
+        return redirect(reverse('shop'))
 
 
 def update_bag(request, product_type, product_id):
-    print(product_type)
     bag = request.session.get('bag', {})
-    quantity = int(request.POST.get('quantity'))
+    if request.method == 'POST':
+        quantity = int(request.POST.get('quantity'))
+        if quantity < 1:
+            messages.error(
+                request,
+                'Click on the remove button to remove item from bag.'
+                )
+            return redirect(reverse('view_bag'))
 
-    if product_type == 'cd' or product_type == 'vinyl':
-        album = get_object_or_404(Album, pk=product_id)
-        bag[album.title][product_type] = quantity
+        if product_type == 'cd' or product_type == 'vinyl':
+            album = get_object_or_404(Album, pk=product_id)
+            bag[album.title][product_type] = quantity
 
-    if product_type == 'S' or product_type == 'M' or product_type == 'L':
-        product = get_object_or_404(Product, pk=product_id)
-        bag[product.name][product_type] = quantity
+        if product_type == 'S' or product_type == 'M' or product_type == 'L':
+            product = get_object_or_404(Product, pk=product_id)
+            bag[product.name][product_type] = quantity
 
-    if product_type == 'other':
-        product = get_object_or_404(Product, pk=product_id)
-        bag[product.name] = quantity
+        if product_type == 'other':
+            product = get_object_or_404(Product, pk=product_id)
+            bag[product.name] = quantity
 
-    request.session['bag'] = bag
-    messages.success(request, 'Item successfully updated in your bag.')
-    return redirect(reverse('view_bag'))
+        request.session['bag'] = bag
+        messages.success(request, 'Item successfully updated in your bag.')
+        return redirect(reverse('view_bag'))
+    else:  # Request method is not post, redirect request
+        return redirect(reverse('view_bag'))
 
 
 def remove_item(request, product_type, product_id):
