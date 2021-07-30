60ae84f0ee301f3adf5dc47a3b98a57d64bbbd52
richardthorp <richardthorp@hotmail.co.uk>
2021-07-30 16:00:04 +0000

    Add DEBUG if else statement

--- radiohead/settings.py
+++ radiohead/settings.py
@@ -26,7 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if 'DEVELOPMENT' in os.environ:
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = ['radiohead-ms4.herokuapp.com', 'localhost']
 
