debce2f39a08af04060197f84e6aab22e7fd1fe8
richardthorp <richardthorp@hotmail.co.uk>
2021-08-05 14:43:12 +0000

    Set up stripe webhook handler and function

---
+++ checkout/webhook_handler.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse
+
+
+# Webhook handler copied from Boutique Ado project
+class StripeWH_Handler:
+    def __init__(self, request):
+        self.request = request
+
+    def handle_event(self, event):
+        # Handle a generic/unknown/unexpected webhook
+        return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200
+        )
+
+    def handle_payment_intent_succeeded(self, event):
+        # Handle a payment_intent.succeeded webhook
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200
+        )
+
+    def handle_payment_intent_failed(self, event):
+        # Handle a payment_intent.failed webhook
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200
+        )
---
+++ checkout/webhooks.py
@@ -0,0 +1,59 @@
+import json
+import stripe
+from django.conf import settings
+from django.http import HttpResponse
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
+from .webhook_handler import StripeWH_Handler
+
+
+# Webhook function copied from Boutique Ado project
+@require_POST
+@csrf_exempt
+def webhook(request):
+    # Listen for webhooks from Stripe
+    wh_secret = settings.STRIPE_WEBHOOK_SECRET
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+
+    payload = request.body
+    event = None
+
+    try:
+        event = stripe.Event.construct_from(
+            json.loads(payload), stripe.api_key, wh_secret
+        )
+    except ValueError as e:
+        # Invalid payload
+        return HttpResponse(status=400)
+
+
+    # Set up webhook handler
+    handler = StripeWH_Handler(request)
+
+    # Map webhook events to relevant handler functions
+    event_map = {
+        'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
+        'payment_intent.failed': handler.handle_payment_intent_failed,
+    }
+
+    # Get the event type from Stripe
+    event_type = event['type']
+
+    event_handler = event_map.get(event_type, handler.handle_event)
+
+    response = event_handler(event)
+
+    return response
+
+    # Handle the event
+    # if event.type == 'payment_intent.succeeded':
+    #     payment_intent = event.data.object # contains a stripe.PaymentIntent
+    #     print('PaymentIntent was successful!')
+    # elif event.type == 'payment_method.attached':
+    #     payment_method = event.data.object # contains a stripe.PaymentMethod
+    #     print('PaymentMethod was attached to a Customer!')
+    # # ... handle other event types
+    # else:
+    #     print('Unhandled event type {}'.format(event.type))
+
+    # return HttpResponse(status=200)
