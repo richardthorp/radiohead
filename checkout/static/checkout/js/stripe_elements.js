72309f15a1994aad0f0e3172d28e2e4042226dc4
richardthorp <richardthorp@hotmail.co.uk>
2021-08-05 15:25:51 +0000

    Add cache checkout data view, url and js

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -20,7 +20,9 @@ card.mount("#card-element");
 
 // Listen to change events on the card Element and display any errors in the
 // card-errors div
-card.on('change', function ({error}) {
+card.on('change', function ({
+    error
+}) {
     if (error) {
         $('#card-errors').text(error.message);
     } else {
@@ -37,17 +39,43 @@ form.addEventListener('submit', function (ev) {
     card.update({
         'disabled': true
     });
-    $("#card-button").attr('disabled', true)
+    $("#card-button").attr('disabled', true);
     $("#loading-overlay").fadeToggle(100);
 
     // Get the client secret from the form data-secret attribute
     const clientSecret = form.dataset.secret;
-    stripe.confirmCardPayment(clientSecret, {
-        payment_method: {
-            card: card,
-            billing_details: {
+    // Did the user check the save info box?
+    const saveDetails = 'save_details' in form;
+    const csrfToken = $("input[name=csrfmiddlewaretoken]").val();
+
+    const postData = {
+        'csrfmiddlewaretoken': csrfToken,
+        'client_secret': clientSecret,
+        'save_details': saveDetails,
+    };
+
+    const url = '/checkout/cache_checkout_data/';
+
+    $.post(url, postData).done(function () {
+        stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+                billing_details: {
+                    name: $.trim(form.name.value),
+                    email: $.trim(form.email.value),
+                    phone: $.trim(form.phone_number.value),
+                    address: {
+                        city: $.trim(form.town_or_city.value),
+                        country: $.trim(form.country.value),
+                        line1: $.trim(form.address_line1.value),
+                        line2: $.trim(form.address_line2.value),
+                        postal_code: $.trim(form.postcode.value),
+                        state: $.trim(form.county.value)
+                    }
+                }
+            },
+            shipping: {
                 name: $.trim(form.name.value),
-                email: $.trim(form.email.value),
                 phone: $.trim(form.phone_number.value),
                 address: {
                     city: $.trim(form.town_or_city.value),
@@ -55,35 +83,28 @@ form.addEventListener('submit', function (ev) {
                     line1: $.trim(form.address_line1.value),
                     line2: $.trim(form.address_line2.value),
                     postal_code: $.trim(form.postcode.value),
-                    state: $.trim(form.county.value)
+                    state: $.trim(form.county.value),
                 }
             }
-        },
-        shipping: {
-            name: $.trim(form.name.value),
-            phone: $.trim(form.phone_number.value),
-            address: {
-                city: $.trim(form.town_or_city.value),
-                country: $.trim(form.country.value),
-                line1: $.trim(form.address_line1.value),
-                line2: $.trim(form.address_line2.value),
-                postal_code: $.trim(form.postcode.value),
-                state: $.trim(form.county.value),
+        }).then(function (result) {
+            if (result.error) {
+                // Show error to your customer (e.g., insufficient funds)
+                $("#card-errors").text(result.error.message);
+                // Unblock the card element and form submit button to allow user to fix error
+                card.update({
+                    'disabled': false
+                });
+                $("#card-button").attr('disabled', false);
+                $("#loading-overlay").fadeToggle(100);
+            } else {
+                // The payment has been processed!
+                if (result.paymentIntent.status === 'succeeded') {
+                    form.submit();
+                }
             }
-        }
-    }).then(function (result) {
-        if (result.error) {
-            // Show error to your customer (e.g., insufficient funds)
-            $("#card-errors").text(result.error.message);
-            // Unblock the card element and form submit button to allow user to fix error
-            card.update({'disabled': false});
-            $("#card-button").attr('disabled', false);
-            $("#loading-overlay").fadeToggle(100);
-        } else {
-            // The payment has been processed!
-            if (result.paymentIntent.status === 'succeeded') {
-                form.submit();
-            }
-        }
-    });
+        });
+    }).fail(function(){
+        // Reload the page to display error message
+        location.reload();
+    })
 });
--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+import json
+from django.shortcuts import render,HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.shortcuts import reverse, redirect
 from django.conf import settings
@@ -8,6 +10,27 @@ from bag.contexts import bag_details
 from .forms import OrderForm
 from .models import Order, ProductOrderLineItem, AlbumOrderLineItem
 from shop.models import Album, Product
+
+
+# cache_checkout_data function copied from Boutique Ado project
+@require_POST
+def cache_checkout_data(request):
+    try:
+        # Get the payment ID from the client secret from Stripe
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+
+        # Call the Payment_intent.modify method and add the form data
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag')),
+            'save_details': request.POST.get('save_details'),
+            'user': request.user,
+        })
+
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Something went wrong! Please try your payment again.')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
