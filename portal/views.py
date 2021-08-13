eef782b708157269eee6f9720e3af6c54531b3a9
richardthorp <richardthorp@hotmail.co.uk>
2021-08-13 12:12:34 +0000

    Add wh and sign up logic to subscriptions

--- portal/views.py
+++ portal/views.py
@@ -18,7 +18,8 @@ def create_portal_customer(request):
     try:
         user_profile = request.user.profile
         if not user_profile.portal_cust_id:
-            # Create a new customer object
+            # If the user doesn't have an existing stripe customer ID,
+            # create a new customer object
             customer = stripe.Customer.create(
                 email=email
                 )
@@ -29,7 +30,25 @@ def create_portal_customer(request):
         customer_id = request.user.profile.portal_cust_id
         price_id = settings.SUBSCRIPTION_PRICE_ID
 
-        # CHECK HERE FOR AN ACITVE SUBSCRIPTION AND REDIRECT TO PORTAL CONTENT IF SO
+        # Check whether the user already has a subscription ID with Stripe
+        if user_profile.subscription_id:
+            try:
+                subscription = stripe.Subscription.retrieve(
+                    user_profile.subscription_id
+                    )
+                # If the subscription is active, redirect user to
+                # Portal Content
+                if subscription.status == 'active':
+                    return redirect(reverse('shop'))  # NEED TO CHANGE THIS TO PORTAL CONTENT
+            # If Stripe returns anything but an active subscription, delete the
+            # Subscription ID on the user profile and create a new Subscription
+                else:
+                    user_profile.subscription_id = ""
+                    user_profile.save()
+            except Exception:
+                user_profile.subscription_id = ""
+                user_profile.save()
+
         subscription = stripe.Subscription.create(
             customer=customer_id,
             items=[{
@@ -37,7 +56,13 @@ def create_portal_customer(request):
             }],
             payment_behavior='default_incomplete',
             expand=['latest_invoice.payment_intent'],
+            metadata={'email': request.user.email}
         )
+
+        # Save the Subscription ID on the Profile model to use for changing
+        # or deleting subscription
+        user_profile.subscription_id = subscription.id
+        user_profile.save()
 
         context = {
             'subscription_id': subscription.id,
@@ -46,21 +71,23 @@ def create_portal_customer(request):
             'stripe_public_key': stripe_public_key,
 
         }
-        request.session['requestsubscriptionId'] = subscription.id,
-        request.session['clientSecret'] = (subscription.latest_invoice.
-                                           payment_intent.client_secret)
+        # request.session['requestsubscriptionId'] = subscription.id,
+        # request.session['clientSecret'] = (subscription.latest_invoice.
+        #                                    payment_intent.client_secret)
 
         return render(request, 'portal/portal_sign_up.html', context)
 
     except Exception as e:
+        print(e)
         messages.error(request, 'Sorry, there was an issue generating the new subscription, \
             "please try again later')
-        redirect(reverse('portal_info'))
+        return redirect(reverse('portal_info'))
 
 
 def portal_sign_up(request):
-    context = {
-        'client_secret': request.session.get('clientSecret'),
-        'subscription_id': request.session.get('requestsubscriptionId'),
-    }
+    # context = {
+    #     'client_secret': request.session.get('clientSecret'),
+    #     'subscription_id': request.session.get('requestsubscriptionId'),
+    # }
+    # print('SUB ID: ', request.session.get('requestsubscriptionId'))
     return render(request, 'portal/portal_sign_up.html', context)
--- portal/webhooks.py
+++ portal/webhooks.py
@@ -3,8 +3,10 @@ import stripe
 from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
+from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
 from profiles.models import Profile
+
 
 @require_POST
 @csrf_exempt
@@ -24,46 +26,62 @@ def portal_webhook(request):
 
     data_object = data['object']
 
-    if event_type == 'customer.subscription.updated':
-        # Get the user profile associated with the Stripe customer ID
-        try:
-            profile = Profile.objects.get(portal_cust_id=data_object['customer'])
-        except Profile.DoesNotExist:
-            return HttpResponse(
-                content='Error finding User in DB',
-                status=500
-            )
-        profile.subscription_status = 'active'
-        profile.save()
+    if event_type == 'customer.subscription.created':
+        # print(data)
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200
         )
 
-    if event_type == 'invoice.payment_failed':
+    elif event_type == 'customer.subscription.updated':
+        # If the user has not completed the subscription payment within 23
+        # hours, the subscription object expires -
+        # delete the subscription object
+        # if data_object['status'] == 'incomplete_expired':
+        #     stripe.SubscriptionItem.delete(
+        #         data_object['id'],
+        #         )
+
+        user = User.objects.get(email=data_object['metadata']['email'])
+        if data_object['status'] == 'active':
+            # The subscription is active - set the the user profile
+            # subsctiption_status to active to allow access to Portal
+            # try:
+            #     profile = Profile.objects.get(
+            #         portal_cust_id=data_object['customer']
+            #         )
+            # except Profile.DoesNotExist:
+            #     return HttpResponse(
+            #         content='Error finding User in DB',
+            #         status=500
+            #     )
+            user.profile.subscription_status = 'active'
+            user.profile.save()
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200
+        )
+
+    elif event_type == 'invoice.payment_failed':
         # If the payment fails or the customer does not have a valid payment method,
         # an invoice.payment_failed event is sent, the subscription becomes past_due.
         # Use this webhook to notify your user that their payment has
         # failed and to retrieve new card details.
         print('TESTING FAILED PAYMENT')
-        print(data)
-        return HttpResponse(
-            content=f'Webhook received: {event["type"]}',
-            status=200
-        )
-    if event_type == 'customer.subscription.created':
-        print(data)
+        # print(data)
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200
         )
 
-    if event_type == 'customer.subscription.deleted':
+    elif event_type == 'customer.subscription.deleted':
         # handle subscription cancelled automatically based
         # upon your subscription settings. Or if the user cancels it.
-        print(data)
+        # print(data)
+        print('SUB DELETED')
 
-    if event_type == 'invoice.payment_succeeded':
+    # Set the default payment method for recurring charges
+    elif event_type == 'invoice.payment_succeeded':
         if data_object['billing_reason'] == 'subscription_create':
             subscription_id = data_object['subscription']
             payment_intent_id = data_object['payment_intent']
@@ -81,11 +99,15 @@ def portal_webhook(request):
                 status=200
             )
 
-    if event_type == 'invoice.paid':
-        profile.subscription_status = 'active'
-        profile.save()
+    elif event_type == 'invoice.paid':
+        # profile.subscription_status = 'active'
+        # profile.save()
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200
         )
-    # return jsonify({'status': 'success'})
+    else:
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200
+        )
