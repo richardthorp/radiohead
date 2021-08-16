13d289b37566b53dd0522b6e85cd636d1f80ba7b
richardthorp <richardthorp@hotmail.co.uk>
2021-08-16 19:12:41 +0000

    Add content and styles to portal_info page

--- portal/urls.py
+++ portal/urls.py
@@ -5,10 +5,8 @@ from .webhooks import portal_webhook
 urlpatterns = [
     path('', views.portal_info, name='portal_info'),
     path('content/', views.portal_content, name='portal_content'),
-    path('create-portal-customer/', views.create_portal_customer,
+    path('subscribe-to-portal/', views.create_portal_customer,
          name='create_portal_customer'),
-    # path('portal_sign_up/', views.portal_sign_up,
-    #      name='portal_sign_up'),
     path('portal_wh/', portal_webhook, name='portal_webhook'),
     path('update_payment_card/', views.update_payment_card,
          name='update_payment_card'),
