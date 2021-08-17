3d3d64c75026ab4adc36fc4d2ce40e3d8e347f6a
richardthorp <richardthorp@hotmail.co.uk>
2021-08-17 17:25:14 +0000

    Add status codes to error views

--- radiohead/views.py
+++ radiohead/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 
 def handle_404_error(request, exception):
-    return render(request, '404_page.html')
+    return render(request, '404_page.html', status=404)
 
 
 def handle_500_error(request, exception=None):
-    return render(request, '500_page.html')
+    return render(request, '500_page.html', status=500)
