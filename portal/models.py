70db358f8bd7ff534cec1db50c60c806f25be623
richardthorp <richardthorp@hotmail.co.uk>
2021-08-18 11:58:51 +0000

    Add image post template content

--- portal/models.py
+++ portal/models.py
@@ -9,7 +9,7 @@ class PortalTextPost(models.Model):
     slug = models.SlugField(default="", max_length=80)
     lead_image = models.ImageField(blank=False, null=False,
                                    upload_to="portal_images")
-    lead_image_summary = models.CharField(blank=False, max_length=60)
+    lead_image_summary = models.CharField(blank=False, max_length=200)
     text_content = models.TextField(blank=False)
     date_posted = models.DateTimeField(default=timezone.now)
     text = models.BooleanField(default=True, editable=False)
@@ -30,7 +30,7 @@ class PortalVideoPost(models.Model):
     slug = models.SlugField(default="", max_length=80)
     lead_image = models.ImageField(blank=False, null=False,
                                    upload_to="portal_images")
-    lead_image_summary = models.CharField(blank=False, max_length=60)
+    lead_image_summary = models.CharField(blank=False, max_length=200)
     video_url = models.URLField(blank=False, null=False)
     text_content = models.TextField(blank=False)
     date_posted = models.DateTimeField(default=timezone.now)
@@ -52,37 +52,37 @@ class PortalImagesPost(models.Model):
     slug = models.SlugField(default="", max_length=80)
     lead_image = models.ImageField(blank=False, null=False,
                                    upload_to="portal_images")
-    lead_image_summary = models.CharField(blank=False, max_length=60)
-    post_text_content = models.TextField(blank=False)
+    lead_image_summary = models.CharField(blank=False, max_length=200)
+    text_content = models.TextField(blank=False)
     image_1 = models.ImageField(blank=False, null=False,
                                 upload_to="portal_images")
-    image_1_summary = models.CharField(blank=False, max_length=60)
+    image_1_summary = models.CharField(blank=False, max_length=200)
     image_2 = models.ImageField(blank=False, null=False,
                                 upload_to="portal_images")
-    image_2_summary = models.CharField(blank=False, max_length=60)
+    image_2_summary = models.CharField(blank=False, max_length=200)
     image_3 = models.ImageField(blank=True, null=True,
                                 upload_to="portal_images")
-    image_3_summary = models.CharField(blank=True, max_length=60)
+    image_3_summary = models.CharField(blank=True, max_length=200)
     image_4 = models.ImageField(blank=True, null=True,
                                 upload_to="portal_images")
-    image_4_summary = models.CharField(blank=True, max_length=60)
+    image_4_summary = models.CharField(blank=True, max_length=200)
     image_5 = models.ImageField(blank=True, null=True,
                                 upload_to="portal_images")
-    image_5_summary = models.CharField(blank=True, max_length=60)
+    image_5_summary = models.CharField(blank=True, max_length=200)
     image_6 = models.ImageField(blank=True, null=True,
                                 upload_to="portal_images")
-    image_6_summary = models.CharField(blank=True, max_length=60)
+    image_6_summary = models.CharField(blank=True, max_length=200)
     image_7 = models.ImageField(blank=True, null=True,
                                 upload_to="portal_images")
-    image_7_summary = models.CharField(blank=True, max_length=60)
+    image_7_summary = models.CharField(blank=True, max_length=200)
     image_8 = models.ImageField(blank=True, null=True,
                                 upload_to="portal_images")
-    image_8_summary = models.CharField(blank=True, max_length=60)
+    image_8_summary = models.CharField(blank=True, max_length=200)
     date_posted = models.DateTimeField(default=timezone.now)
     images = models.BooleanField(default=True, editable=False)
 
     def __str__(self):
-        return f"Images post: {self.post_title}"
+        return f"Images post: {self.title}"
 
     def save(self, *args, **kwargs):
         if self.slug != slugify(self.title):
