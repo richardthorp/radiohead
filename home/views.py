cd98133b1886fb4ae2ab9e401169c4c67cd37a60
richardthorp <richardthorp@hotmail.co.uk>
2021-08-21 10:17:43 +0000

    Add admin hub template, view and link in base.html

--- home/views.py
+++ home/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 def index(request):
     return render(request, template_name="home/index.html")
+
+
+@login_required
+def admin_hub(request):
+    if not request.user.is_staff:
+        return redirect(reverse('home'))
+    else:
+        return render(request, 'home/admin-hub.html')
